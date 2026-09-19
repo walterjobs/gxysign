aabdf1b10ee52a292256d8dea1250ae4f7f34095
walterjobs <73602943+walterjobs@users.noreply.github.com>
2026-09-19 21:45:31 +0800

    Replace serverchan_sdk with Bark API for notifications

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import os
 import json
 import requests
 from datetime import datetime
-from serverchan_sdk import sc_send
+# from serverchan_sdk import sc_send
 
 # ====== 从环境变量读取（GitHub Secrets）======
 auth = os.environ["MOGUDING_AUTH"]
-send_key = os.environ["SERVERCHAN_KEY"]
+send_key = os.environ["BARK_KEY"]
 planid = os.environ["PLANID"]
 
 list_url = "https://api.moguding.net:9000/attendence/clock/v2/listByApp"
 sign_url = "https://api.moguding.net:9000/msg/notice/v1/batchSignMessage"
+
 
 headers = {
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 "
@@ -42,6 +43,8 @@ stu_name_list = [
 
 desp = "\n".join(stu_name_list) + "\n未签到" if stu_name_list else "全部已签到 ✅"
 print (desp)
+bark_url = f"https://api.day.app/{send_key}/{desp}"
+
 # ====== 2. 签到（无论何时都执行） ======
 for user_id in user_id_list:
     param2 = {"type": 1, "tolds": user_id}
@@ -60,7 +63,7 @@ start = bj_time.replace(hour=21, minute=30, second=0, microsecond=0)
 end   = bj_time.replace(hour=22, minute=30, second=0, microsecond=0)
 
 if start <= bj_time <= end:
-    response = sc_send(send_key, desp)
+    response = requests.post(bark_url, timeout=10)
     print("已推送：\n", desp)
 else:
     print(f"当前北京时间 {bj_time.strftime('%H:%M:%S')} 不在 22:00-22:15 范围内，跳过推送。")
